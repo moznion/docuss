9599c5ec77151e67640b072d28d09e220b10f6d0
moznion <moznion@gmail.com>
2016-07-26 22:01:38 +0900

    Tiny fix

--- src/main/java/net/moznion/docuss/presenter/StandardOutPresenter.java
+++ src/main/java/net/moznion/docuss/presenter/StandardOutPresenter.java
@@ -6,6 +6,6 @@ import net.moznion.docuss.formatter.DocussFormatterGenerator;
 public class StandardOutPresenter implements DocussPresenter {
     @Override
     public void out(DocussFormatterGenerator formatterGenerator, DocussDocument response) {
-        System.out.println(formatterGenerator.getFormatterGenerator().apply(response));
+        System.out.print(formatterGenerator.getFormatterGenerator().apply(response));
     }
 }
